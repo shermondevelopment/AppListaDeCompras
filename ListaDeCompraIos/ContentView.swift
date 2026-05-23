d039bd42b7236432819c12da757993884fc3d054
Vitor shermon <victorshermon@yahoo.com>
2026-05-23 12:52:07 -0300

    Initial Commit

---
+++ ListaDeCompraIos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ListaDeCompraIos
+//
+//  Created by VITOR SHERMON on 23/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ListaDeCompraIos/ListaDeCompraIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListaDeCompraIosApp.swift
+//  ListaDeCompraIos
+//
+//  Created by VITOR SHERMON on 23/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ListaDeCompraIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
